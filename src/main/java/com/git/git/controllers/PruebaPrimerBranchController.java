badd8a7022984e2a1694cc62f59e545333a065e1
hector gonzalez <hgonzalez@cleveritgroup.com>
2021-07-17 11:24:51 -0400

    commit primer branch

---
+++ src/main/java/com/git/git/controllers/PruebaPrimerBranchController.java
@@ -0,0 +1,7 @@
+package com.git.git.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/pruebaPrimera")
+public class PruebaPrimerBranchController {
+}
