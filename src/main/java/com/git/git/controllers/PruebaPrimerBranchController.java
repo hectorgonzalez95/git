79ab518203fd32156cdf0f94e7cf293b4d661b3b
hector gonzalez <hgonzalez@cleveritgroup.com>
2021-07-17 11:31:04 -0400

    agrego resttemplate

--- src/main/java/com/git/git/controllers/PruebaPrimerBranchController.java
+++ src/main/java/com/git/git/controllers/PruebaPrimerBranchController.java
@@ -1,8 +1,13 @@
 package com.git.git.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("/pruebaPrimera")
 public class PruebaPrimerBranchController {
   private final RestTemplate restTemplate;
+
+    public PruebaPrimerBranchController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 }
