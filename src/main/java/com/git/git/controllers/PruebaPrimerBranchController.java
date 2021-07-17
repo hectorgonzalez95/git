cb196f6eac74f5f3f3703cc52c99ecfd0d4eefba
hectorgonzalez95 <hectorgonzalez95@hotmail.es>
2021-07-17 11:28:51 -0400

    Agrego resttemplate

--- src/main/java/com/git/git/controllers/PruebaPrimerBranchController.java
+++ src/main/java/com/git/git/controllers/PruebaPrimerBranchController.java
@@ -4,4 +4,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pruebaPrimera")
 public class PruebaPrimerBranchController {
+  private final RestTemplate restTemplate;
 }
